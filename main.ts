5c77f43459ae43ef4eff94dfa533043e86c079c0
artissieclark <clarksimone77@gmail.com>
2025-02-13 10:42:27 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,13 @@
-player.onTravelled(WALK, function () {
+player.onChat("run", function () {
 	
 })
-player.onChat("run", function () {
-    blocks.fill(
-    SLIME_BLOCK,
-    pos(20, 0, 20),
-    pos(20, 0, 20),
-    FillOperation.Replace
-    )
-    mobs.spawn(PIG, pos(0, 0, 0))
+player.onTravelled(FLY, function () {
+	
 })
+blocks.fill(
+SLIME_BLOCK,
+pos(20, 0, 20),
+pos(20, 0, 20),
+FillOperation.Replace
+)
+mobs.spawn(PIG, pos(0, 0, 0))
